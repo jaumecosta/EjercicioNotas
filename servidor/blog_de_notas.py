164b981160132fe172bc4a536a08d41dab6ca505
Jaume Costa Gomez <jaucosta@hotelbeds.com>
2019-09-11 12:08:43 +0200

    Iniciar bloc de notas

---
+++ servidor/blog_de_notas.py
@@ -0,0 +1,16 @@
+"""
+    blog de notas de ()
+    ----------------------------------
+    v1.0
+    ---------------------------------------------
+    -----------------------------------------------
+    Ejercicio 3pts para el examen final del modulo
+    Sin internet
+    No hay slack
+    Para terminal
+    Pensar bien las opciones que tendra el bloc de notas y que de tiempo a termminarlas
+    Diagramacion en www.lucidchart.co (diagrama flujo)
+    Entorno virtaul python 3.7.4
+    git local 
+    Finalizar el ejercicio a las 16:15 enviar enlace por git hub
+"""
